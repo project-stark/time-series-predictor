6a3875d99a09e5f1dd1a3121d53f1d8a73344c83
Divyesh Peshavaria <divyeshpeshavaria@gmail.com>
2017-02-05 04:01:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ from distutils.core import setup
 setup(
     name='ts_predictor',
     version='0.1',
+    packages=['ts_predictor'],
     url='https://github.com/project-stark/time-series-predictor',
     license='',
     author='Divyesh Peshavaria',
     author_email='divyeshpeshavaria@gmail.com',
     description='',
-    requires=[
-        'sklearn',
+    install_requires=[
+        'numpy',
+        'scipy',
         'pandas',
-        'numpy'
+        'sklearn'
     ]
 )
