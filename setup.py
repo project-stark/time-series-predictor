80e013fa43f2cf5f6cab2c9b877b09e9bd0423b9
Divyesh Peshavaria <divyeshpeshavaria@gmail.com>
2017-02-05 19:55:08 +0530

    Bump to version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ts_predictor',
-    version='0.1',
+    version='0.2',
     packages=['ts_predictor'],
     url='https://github.com/project-stark/time-series-predictor',
     license='',
