d2fa08c75ebba2047b1d5179be353802ca5a59c0
Divyesh Peshavaria <divyeshpeshavaria@gmail.com>
2017-02-05 19:54:28 +0530

    Give variable names as per convention, Convert float numpy array to int before applying inverse_transform

--- ts_predictor/ts_predictor.py
+++ ts_predictor/ts_predictor.py
@@ -6,17 +6,19 @@ from sklearn.preprocessing import LabelEncoder
 
 class TimeSeriesPredictor:
     def __init__(self, entity_id):
-        self.entity_id = entity_id
-        self.clf = DecisionTreeRegressor()
-        self.encoder = LabelEncoder()
+        self._entity_id = entity_id
+        self._clf = DecisionTreeRegressor()
+        self._encoder = LabelEncoder()
 
     def fit(self, data):
-        data = pd.DataFrame(data)
-        data[["state"]] = self.encoder.fit_transform(np.ravel(data[["state"]]))
-        self.clf.fit(data[["timestamp"]], data[["state"]])
+        df = pd.DataFrame(data)
+        df[["state"]] = self._encoder.fit_transform(np.ravel(df[["state"]]))
+        self._clf.fit(df[["timestamp"]], df[["state"]])
 
     def predict(self, arg):
-        return self.encoder.inverse_transform(self.clf.predict(pd.Series(arg)))
+        result = self._clf.predict(pd.Series(arg).values.reshape(-1, 1))
+        int_result = np.array(result, dtype="int32")
+        return self._encoder.inverse_transform(int_result)[0]
 
     def __repr__(self):
-        return "Time Series Predictor for device {}".format(self.entity_id)
+        return "Time Series Predictor for device {}".format(self._entity_id)
