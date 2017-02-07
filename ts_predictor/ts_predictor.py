1b756fc7374b109e94a45aba71c6ee278a811a33
Divyesh Peshavaria <divyeshpeshavaria@gmail.com>
2017-02-07 08:28:28 +0530

    Replace DecisionTreeRegressor with LogisticRegression classifier, Remove print statement used for logging

--- ts_predictor/ts_predictor.py
+++ ts_predictor/ts_predictor.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LogisticRegression
 
 
 class TimeSeriesPredictor:
@@ -8,7 +8,7 @@ class TimeSeriesPredictor:
         self._classes = list(set(states))
         self._states = list(map(lambda state: self._classes.index(state), states))
         self._timestamps = list(map(lambda x: x["timestamp"], data))
-        self._clf = DecisionTreeRegressor()
+        self._clf = LogisticRegression(solver="sag", multi_class="multinomial", max_iter=10000)
 
     def fit(self, data_point):
         if data_point["state"] not in self._classes:
@@ -16,7 +16,6 @@ class TimeSeriesPredictor:
 
         self._states.append(self._classes.index(data_point["state"]))
         self._timestamps.append(data_point["timestamp"])
-        print(np.array(list(self._timestamps)).reshape(len(self._timestamps), 1))
         self._clf.fit(np.array(list(self._timestamps)).reshape(len(self._timestamps), 1), np.array(self._states))
 
     def predict(self, arg):
