26915e04c4fb1e9967a91309bcd899a138b112f5
Divyesh Peshavaria <divyeshpeshavaria@gmail.com>
2017-02-11 14:59:00 +0530

    Fix #1, Change input format to {timestamp: state}

--- ts_predictor/ts_predictor.py
+++ ts_predictor/ts_predictor.py
@@ -4,19 +4,12 @@ from sklearn.linear_model import LogisticRegression
 
 class TimeSeriesPredictor:
     def __init__(self, data):
-        states = list(map(lambda x: x["state"], data))
+        states = data.values()
         self._classes = list(set(states))
         self._states = list(map(lambda state: self._classes.index(state), states))
-        self._timestamps = list(map(lambda x: x["timestamp"], data))
-        self._clf = LogisticRegression(solver="sag", multi_class="multinomial", max_iter=10000)
+        self._timestamps = list(data.keys())
+        self._clf = LogisticRegression(solver="sag", multi_class="multinomial", max_iter=10000, warm_start=True)
+        self._clf.fit(np.array(self._timestamps).reshape(len(self._timestamps), 1), np.array(self._states))
 
-    def fit(self, data_point):
-        if data_point["state"] not in self._classes:
-            self._classes.append(data_point["state"])
-
-        self._states.append(self._classes.index(data_point["state"]))
-        self._timestamps.append(data_point["timestamp"])
-        self._clf.fit(np.array(list(self._timestamps)).reshape(len(self._timestamps), 1), np.array(self._states))
-
-    def predict(self, arg):
-        return self._classes[int(self._clf.predict(np.array(arg)))]
+    def predict(self, timestamp):
+        return self._classes[int(self._clf.predict(np.array(timestamp)))]
