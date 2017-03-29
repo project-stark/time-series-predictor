11b2ec2e42022af43aabe3f1dcfe88726dd1dcc6
Divyesh Peshavaria <divyeshpeshavaria@gmail.com>
2017-03-29 22:42:28 +0530

    Increase number of features Use hours and minutes instead of calculating minutes of day

--- ts_predictor/ts_predictor.py
+++ ts_predictor/ts_predictor.py
@@ -1,24 +1,20 @@
 """ts_predictor.py: Machine learning models to do predictions on Time series data"""
 
-
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder
 
 
 class TimeSeriesPredictor:
 
     def __init__(self, datetimes, states):
 
-        self._encoder = LabelEncoder()
-        self._minutes_of_day = np.array(list(map(lambda dt: (dt.hour * 60) + dt.minute, datetimes)))
-        self._states = self._encoder.fit_transform(states)
+        self._features = np.array([[dt.hour, dt.minute] for dt in datetimes]).reshape(len(datetimes), 2)
+        self._labels = states
 
         self._clf = LogisticRegression(solver="sag", multi_class="multinomial", max_iter=10000, warm_start=True)
-
-        self._clf.fit(self._minutes_of_day.reshape(len(self._minutes_of_day), 1), self._states)
+        self._clf.fit(self._features, self._labels)
 
     def predict(self, datetime_obj):
 
-        return self._encoder.inverse_transform(self._clf.predict(np.array([(datetime_obj.hour * 60) +
-                                                                           datetime_obj.minute]).reshape(1, -1)))[0]
+        data = np.array([[datetime_obj.hour, datetime_obj.minute]])
+        return self._clf.predict(data)
